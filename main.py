a5a27e502bdbcf97c5be6fb6fcaf91c65343a63f
jedixxt <79491133+jedixxt@users.noreply.github.com>
2022-08-11 21:39:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.animation as animation
 
-
 def rossler_system(x, y, z, dx, dy, dz, b, a=0.2, c=5.7):
+    
+    #system
     x_dot = - y - z
     y_dot = x + a * y
     z_dot = b + z * (x - c)
 
+    #system linearized
     dx_dot = - dy - dz
     dy_dot = dx + a * dy
     dz_dot = dz * x + z * dx - c * dz
 
     return x_dot, y_dot, z_dot, dx_dot, dy_dot, dz_dot
 
-
 db = 0.001  # parameter step size
 b = np.arange(0.1, 2, db)  # parameter range
-b_choose = 0.5000000000000003
+b_choose = 0.5000000000000003 # paramter for an specific plot
 dt = 0.01  # time step
 t = np.arange(1, 100, dt)  # time range
 
@@ -27,6 +28,7 @@ xs = np.empty(len(t) + 1)
 ys = np.empty(len(t) + 1)
 zs = np.empty(len(t) + 1)
 
+# initialize solution arrays for the system linearized
 dxs = np.empty(len(t) + 1)
 dys = np.empty(len(t) + 1)
 dzs = np.empty(len(t) + 1)
@@ -35,28 +37,30 @@ dzs = np.empty(len(t) + 1)
 xs[0], ys[0], zs[0] = (0.5, 0.3, -1.0)
 dxs[0], dys[0], dzs[0] = (1.0, 0, 0)
 
-# Save the plot points coordinates and plot the with a single call to plt.plot
-# instead of plotting them one at a time, as it's much more efficient
+# arrays to save the paramters b and the peaks of z (or another axis)
 b_maxes = []
 z_maxes = []
 b_mins = []
 z_mins = []
 
+# arrays for plot the axis with parameter b_choose
 x_3d = np.empty(len(t) + 1)
 y_3d = np.empty(len(t) + 1)
 z_3d = np.empty(len(t) + 1)
 
+# arrays for save the Lyapunov Expoent and related paramter b
 lamb_lyap = []
 b_lyap = []
 
 for B in b:
-    # Print something to show everything is running
-    # print(f"{R=:.2f}")
+
+    # coefficients for the calculation of the Lyapunov Expoent
     K = 0
     lamb = 0.0
 
     for i in range(len(t)):
-        # approximate numerical solutions to system
+
+        # Approximate numerical solutions to system and the system linearized
         x_dot, y_dot, z_dot, dx_dot, dy_dot, dz_dot = rossler_system(xs[i],
                                                                      ys[i], zs[i], dxs[i], dys[i], dzs[i], B)
         xs[i + 1] = xs[i] + (x_dot * dt)
@@ -65,7 +69,8 @@ for B in b:
         dxs[i + 1] = dxs[i] + (dx_dot * dt)
         dys[i + 1] = dys[i] + (dy_dot * dt)
         dzs[i + 1] = dzs[i] + (dz_dot * dt)
-
+        
+        # Calculate the Lyapunov Expoent 
         K += 1
         dist = np.sqrt(dxs[i] * dxs[i] + dys[i] * dys[i] + dzs[i] * dzs[i])
         lamb += np.log(dist / 1.0)
@@ -81,9 +86,9 @@ for B in b:
         y_3d = np.copy(ys)
         z_3d = np.copy(zs)
 
-    # calculate and save the peak values of the z solution
+    # Find the peaks values of the z solution
     for i in range(1, len(zs) - 1):
-        # save the local maxima
+        # Append the peaks and related parameters b on an array
         if zs[i - 1] < zs[i] and zs[i] > zs[i + 1]:
             b_maxes.append(B)
             z_maxes.append(zs[i])
@@ -91,12 +96,13 @@ for B in b:
             b_mins.append(B)
             z_mins.append(zs[i])
 
-    # "use final values from one run as initial conditions for the next to stay near the attractor"
+    # Turn initial conditions of next step as the final points of the last one
     xs[0], ys[0], zs[0] = xs[i], ys[i], zs[i]
     dxs[0], dys[0], dzs[0] = 1, 0, 0
 
-# -- Plots
+#-- Plots
 
+#-- Creat animation and save in a gif
 fig = plt.figure()
 
 ax = fig.add_subplot(111, projection='3d')
@@ -106,18 +112,16 @@ ax.plot(x_3d, y_3d, z_3d, zdir='z',color='black',linewidth=0.5)
 ax.set_xlabel('$x$', fontsize='16.5', horizontalalignment='center')
 ax.set_ylabel('$y$', fontsize='16.5', horizontalalignment='center')
 ax.set_zlabel('$z$', fontsize='16.5', horizontalalignment='center')
-plt.show()
 
 def rotate(angle):
     ax.view_init(azim=angle)
 
-
-print("Making animation")
 rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0, 362, 2), interval=100)
 rot_animation.save('rotation.gif', dpi=80, writer='imagemagick')
 
-
+#-- Graph of y(t) in function of x(t) 
 def x_y(xs, ys):
+
     fig = plt.figure()
 
     a = plt.plot(xs, ys, color="black", linewidth=0.5, label=' a = 0.2\n c = 5.7\n b= 0.5')
@@ -128,8 +132,9 @@ def x_y(xs, ys):
 
     return fig
 
-
+#-- Graph of z(t) in function of x(t) 
 def x_z(xs, zs):
+
     fig = plt.figure()
 
     a = plt.plot(xs, zs, color="black", linewidth=0.5, label=' a = 0.2\n c = 5.7\n b= 0.5')
@@ -140,7 +145,7 @@ def x_z(xs, zs):
 
     return fig
 
-
+#-- Graph of z(t) in function of y(t) 
 def y_z(ys, zs):
     fig = plt.figure()
 
@@ -152,8 +157,9 @@ def y_z(ys, zs):
 
     return fig
 
-
+#-- Bifurcation diagram: peaks (maximums) of z(t) in function of paramter b 
 def z_b(z_maxes, b_maxes):
+
     fig = plt.figure()
 
     a = plt.scatter(b_maxes, z_maxes, color="black", s=1, alpha=1)
@@ -163,8 +169,9 @@ def z_b(z_maxes, b_maxes):
 
     return fig
 
-
+#-- Lyapunov diagram: Graph of Lyapunov Expoent in function of parameter b 
 def lamb_b(lamb_lyap, b_lyap, b):
+
     fig = plt.figure()
 
     a = plt.plot(b_lyap, lamb_lyap, color="black", label=
@@ -177,7 +184,7 @@ def lamb_b(lamb_lyap, b_lyap, b):
 
     return fig
 
-
+#-- Save plots in a PDF file
 fig1 = x_y(x_3d, y_3d)
 fig2 = x_z(x_3d, z_3d)
 fig3 = y_z(y_3d, z_3d)
